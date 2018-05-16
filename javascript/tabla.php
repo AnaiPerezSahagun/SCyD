0be911e8ad93f01e094b4a5cf774544fb6504490
AnaiPerezSahagun <36524387+AnaiPerezSahagun@users.noreply.github.com>
2018-05-16 18:12:01 -0500

    Update tabla.php
    Aquí, principalmente es agregar la imagen para que se borre el contacto, quite el botón y agregue la imagen.
    También quite la columna de contraseñas y solo puse las columnas que quería que se vieran, al igual que la fecha la quite

--- javascript/tabla.php
+++ javascript/tabla.php
@@ -2,11 +2,12 @@
  require_once 'LIGA3/LIGA.php';
  //BD('localhost', 'root', '', 'base');
  $liga = LIGA('base.usuarios');
- $cols = array('*', 'Acciones'=>'<button class="borrar" data="@[0]">X</button>');
+ $cols = array('0','1','3','Acciones'=>'<button class="borrar" data="@[0]"><input type="image" name="valor" value="borrar" src="imagenes/eliminar.png" style= "width:15px; height:10px"></button>');
  
  echo '<label>Filtro: <input id="filtro" name="filtro" /></label>';
+ //<input name="borrar" src="eliminar.png" type="image" />
  
- $props = array('nombre'=>array('class'=>'busca'), 'fecha'=>'class="busca"');
+ $props = array('nombre'=>array('class'=>'busca')/*, 'fecha'=>'class="busca"'*/);
  
  HTML::tabla($liga, 'Usuarios registrados', $cols, $props);
 ?>
